1655f4ee89b76d71270d4573c0bb1beb52630617
dwaware <74695555+dwaware@users.noreply.github.com>
2021-02-14 16:20:06 -0700

    Intantiate via script
    Create an instance of the star model as a prefab via a script.

---
+++ Assets/stars.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stars : MonoBehaviour
+{
+    public GameObject star;
+
+    public List<GameObject> starList = new List<GameObject>();
+
+    Vector3 starScale = new Vector3(1, 1, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = Instantiate(star, new Vector3(0, 0, 0), Quaternion.identity);
+        go.name = "Star";
+        go.transform.localScale = starScale;
+    }
+
+    void Update()
+    {
+
+    }
+}
