74da4dd7e095abe0907d11c0a9cf6a9afa83d7be
dwaware <74695555+dwaware@users.noreply.github.com>
2021-02-15 00:12:24 -0700

    Randomly create GOs
    Randomly create a group of GOs using the new dynamic star model.

--- Assets/stars.cs
+++ Assets/stars.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class stars : MonoBehaviour
 {
-    public GameObject star;
+    public List<GameObject> star = new List<GameObject>();
 
-    public List<GameObject> starList = new List<GameObject>();
-
-    Vector3 starScale = new Vector3(10, 10, 10);
+    Vector3 starScale = new Vector3(5, 5, 5);
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject go = Instantiate(star, new Vector3(0, 0, 0), Quaternion.identity);
-        go.name = "Star";
-        go.transform.localScale = starScale;
+        float max = 250.0f;
+
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject go = Instantiate(star[i], new Vector3(Random.Range(-max, max), Random.Range(-max, max), Random.Range(-max, max)), Quaternion.identity);
+            go.name = "Star "+i;
+            //go.transform.localScale = starScale;
+        }
     }
 
     void Update()
